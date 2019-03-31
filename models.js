7819ff23e21f61439e3ba785f94470f9fda9d86e
James Tyler <macndchase@gmail.com>
2019-03-31 16:07:08 -0400

    heroku changed some stuff i think

--- models.js
+++ models.js
@@ -5,8 +5,12 @@ const mongoose = require("mongoose");
 const blogSchema = mongoose.Schema({
   title: { type: String, required: true} ,
   content: { type: String, required: true},
-  author: { type: String, required: true},
+  author: { firstName: String, lastName: String,  required: true},
   publishedDate: {date: Date}
+});
+
+blogSchema.virtual("authorString").get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
 blogSchema.method.serialize = function(){
